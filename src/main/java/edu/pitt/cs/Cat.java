336dc7dd14257b7662c1bd394aa83e9ed6dcfaec
nij-patel <nijpatel28@gmail.com>
2024-04-02 16:46:55 -0400

    real tests

--- src/main/java/edu/pitt/cs/Cat.java
+++ src/main/java/edu/pitt/cs/Cat.java
@@ -1,7 +1,7 @@
 package edu.pitt.cs;
 
 import org.mockito.Mockito;
-import static org.mockito.Mockito.*; 
+import static org.mockito.Mockito.*;
 
 public interface Cat {
 	public static Cat createInstance(InstanceType type, int id, String name) {
@@ -13,17 +13,21 @@ public interface Cat {
 			case SOLUTION:
 				return new CatSolution(id, name);
 			case MOCK:
-			    // TODO: Return a mock object that emulates the behavior of a real object.
-				return null;
+				CatSolution c = Mockito.mock(CatSolution.class);
+				Mockito.when(c.toString()).thenReturn("ID " + id + ". " + name);
+				Mockito.when(c.getId()).thenReturn(id);
+				Mockito.when(c.getRented()).thenReturn(false);
+				Mockito.when(c.getName()).thenReturn(name);
+				return c;
 			default:
-				assert(false);
+				assert (false);
 				return null;
 		}
 	}
 
 	// WARNING: You are not allowed to change any part of the interface.
 	// That means you cannot add any method nor modify any of these methods.
-	
+
 	public void rentCat();
 
 	public void returnCat();
--- src/main/java/edu/pitt/cs/RentACatImpl.java
+++ src/main/java/edu/pitt/cs/RentACatImpl.java
@@ -18,8 +18,16 @@ public class RentACatImpl implements RentACat {
 	 */
 
 	public boolean returnCat(int id) {
-		// TODO: Fill in
-		return false;
+		Cat cat = getCat(id);
+		if(cat != null && cat.getRented()){
+			cat.returnCat();
+			System.out.println("Welcome back, " + cat.getName() + "!");
+			return true;
+		}
+		else{
+			System.out.println(cat.getName() + " is already here!");
+			return false;
+		}
 	}
 
 	/**
@@ -33,8 +41,15 @@ public class RentACatImpl implements RentACat {
 	 */
 
 	public boolean rentCat(int id) {
-		// TODO: Fill in
-		return false;
+		Cat cat = getCat(id);
+		if(cat != null && !cat.getRented()){
+			cat.rentCat();
+			System.out.println(cat.getName() + " has been rented.");
+			return true;
+		}else {
+			System.out.println("Sorry, " + cat.getName() + " is not here!");
+			return false;
+		}
 	}
 
 	/**
@@ -47,8 +62,12 @@ public class RentACatImpl implements RentACat {
 	 */
 
 	public boolean renameCat(int id, String name) {
-		// TODO: Fill in
-		return false;
+		Cat cat = getCat(id);
+		if(cat != null){
+			cat.renameCat(name);
+			return true;
+		}
+		else return false;
 	}
 
 	/**
@@ -62,8 +81,16 @@ public class RentACatImpl implements RentACat {
 	 */
 
 	public String listCats() {
-		// TODO: Fill in
-		return "WRITE CODE FOR THIS";
+		String listOfAvailableCats = "";
+		for(int i = 1; i <= cats.size(); i++){
+			Cat cat = getCat(i);
+			if(cat != null) {
+				if(!cat.getRented()){
+					listOfAvailableCats += cat.toString() + "\n";
+				}
+			}
+		}
+		return listOfAvailableCats;
 	}
 
 	/**
